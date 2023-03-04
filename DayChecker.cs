2df558041059657d71fa4b728b5371d892a3b9ba
beansandyaks <gitbinayakmaharjan@gmail.com>
2023-03-04 17:55:20 +0545

    Days of Week Checker

---
+++ DayChecker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DaysOfWeek;
+
+public class DateChecker
+{
+    private readonly int[] _daysOfWeek;
+    private readonly DateTime _startDate;
+    private readonly DateTime _endDate;
+
+    //initialize daysofWeek array, startDate and endDate in constructor
+    public DateChecker(int[] daysOfWeek, DateTime startDate, DateTime endDate)
+    {
+        _daysOfWeek = daysOfWeek;
+        _startDate = startDate;
+        _endDate = endDate;
+    }
+
+    //Method to print number of occurences
+    public void PrintOccurrences()
+    {
+        foreach (int dayOfWeek in _daysOfWeek)
+        {
+            int occurrences = 0;
+            //https://learn.microsoft.com/en-us/powerquery-m/date-adddays
+            for (DateTime date = _startDate; date <= _endDate; date = date.AddDays(1))
+            {
+                //https://stackoverflow.com/questions/9199080/how-to-get-the-integer-value-of-day-of-week
+                if ((int)date.DayOfWeek == dayOfWeek)
+                {
+                    occurrences++;
+                    Console.WriteLine($"{date.DayOfWeek} ({date:d})");
+                }
+            }
+            Console.WriteLine($"{occurrences} occurrences of {(DayOfWeek)dayOfWeek}");
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DaysOfWeek;
+
+class DayChecker
+{
+    static void Main(string[] args)
+    {
+        //Take input for days of week where 0 represents Sunday and 6 represents Saturday
+        //https://learn.microsoft.com/en-us/powerquery-m/day-type
+        Console.WriteLine("Enter a list of integers seperated by commas representing the days of week(0-6): ");
+        string daysOfWeek = Console.ReadLine(); ;
+        int[] listOfDays = Array.ConvertAll(daysOfWeek.Split(','), int.Parse);
+
+        Console.WriteLine("Start Date: ");
+        DateTime startDate = DateTime.Parse(Console.ReadLine());
+
+        Console.WriteLine("End Date: ");
+        DateTime endDate = DateTime.Parse(Console.ReadLine());
+
+        var dayChecker = new DateChecker(listOfDays, startDate, endDate);
+        dayChecker.PrintOccurrences();
+
+        Console.ReadLine();
+
+    }
+}
